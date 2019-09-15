42953e978e190639e8f94348d29327494eb1f125
Manu de Quevedo <hola@manudequevedo.com>
2019-09-14 22:41:05 -0500

    Added initial components

---
+++ src/components/footer/footer.component.js
@@ -0,0 +1,27 @@
+import React from "react"
+import { connect } from "react-redux"
+import footerLang from "./footer.lang.json"
+import "./footer.styles.css"
+
+const mapStateToProps = state => {
+  return {
+    lang: state.get("lang").get("value")
+  }
+}
+
+export default connect(mapStateToProps)(props => {
+  return (
+    <React.Fragment>
+      <div className="footer-bottom base-grid">
+        <div className="content-basic">
+          <div>
+            <div className="footer-copyrights">
+              © {new Date().getFullYear()} - <span className="footer-dev">Manu de Quevedo, </span>
+              {footerLang.header.rights[props.lang]}
+            </div>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  )
+})
---
+++ src/components/navbar/navbar.component.js
@@ -0,0 +1,136 @@
+import React, { useState, useEffect } from "react"
+import { NavLink } from "react-router-dom"
+import store from "../../store"
+import { connect } from "react-redux"
+import navLang from "./navbar.lang.json"
+import "./navbar.styles.css"
+
+const checkActive = (match, location) => {
+  //some additional logic to verify you are in the home URI
+  if (!location) return false
+  const { pathname } = location
+  return pathname === "/"
+}
+
+const mapStateToProps = state => {
+  return {
+    lang: state.get("lang").get("value")
+  }
+}
+
+// Set up the languages and code to be recognized
+const allLanguages = [
+   { code: "es", name: "Español" },
+   { code: "en", name: "English" }
+]
+
+export default connect(mapStateToProps)(props => {
+  const [isTop, setIsTop] = useState(true)
+  // Get all language options to be displayed on the <select> tag
+  const options = allLanguages.map(language => {
+    return <option value={language.code}>{language.name}</option>
+  })
+
+  document.addEventListener("scroll", () => {
+    if ((window.scrollY === 0) !== isTop) {
+      setIsTop(window.scrollY === 0)
+    }
+  })
+
+  useEffect(() => {
+    setIsTop(window.scrollY === 0)
+  }, [])
+
+  useEffect(() => {
+    const menu = document.getElementById("menu-responsive")
+    const menuBurger = document.getElementById("burger-buttom")
+    menuBurger.addEventListener(
+      "click",
+      e => {
+        let isActive = menuBurger.classList.contains("is-active")
+        if (isActive) {
+          menuBurger.classList.remove("is-active")
+          menu.classList.remove("is-active")
+        } else {
+          menuBurger.classList.add("is-active")
+          menu.classList.add("is-active")
+        }
+      },
+      []
+    )
+
+    menu.addEventListener("click", e => {
+      menuBurger.classList.remove("is-active")
+      menu.classList.remove("is-active")
+    })
+  }, [])
+
+  return (
+    <React.Fragment>
+      <nav className={`top-menu ${isTop ? "" : "no-top"}`}>
+        <div className="top-menu-wrapper">
+          <div className="logo" />
+          <div className="hamburger--3dxy" id="burger-buttom">
+            <span className="hamburger-box">
+              <span className="hamburger-inner" />
+            </span>
+          </div>
+          <div className="menuItems">
+            <NavLink to="/">{navLang.menu.home[props.lang]}</NavLink>
+            <NavLink to="/about">
+              <span>{navLang.menu.about.main[props.lang]}</span>
+            </NavLink>
+            <NavLink to="/projects">
+              {navLang.menu.projects[props.lang]}
+            </NavLink>
+            <NavLink to="/contact">
+              <span>{navLang.menu.contact.main[props.lang]}</span>
+            </NavLink>
+            <div className="lang-select">
+              <select
+                onChange={e => {
+                  store.dispatch({
+                    type: "TOGGLE-LANG",
+                    language: e.target.value
+                  });
+                }}
+              >
+                {options}
+              </select>
+            </div>
+          </div>
+        </div>
+        <div className="menuItems-responsive" id="menu-responsive">
+          <div>
+            <div className="menuItems-top-margin">
+              <NavLink to="/" isActive={checkActive}>
+                {navLang.menu.home[props.lang]}
+              </NavLink>
+              <NavLink to="/about">
+                {navLang.menu.about.main[props.lang]}
+              </NavLink>
+              <NavLink to="/projects">
+                {navLang.menu.projects[props.lang]}
+              </NavLink>
+              <NavLink to="/contact">
+                {navLang.menu.contact.main[props.lang]}
+              </NavLink>
+              <div className="lang-select">
+                <select
+                  onChange={e => {
+                    store.dispatch({
+                      type: "TOGGLE-LANG",
+                      language: e.target.value
+                    });
+                  }}
+                >
+                  {options}
+                </select>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </React.Fragment>
+  );
+})
