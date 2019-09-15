af11be4b91a7a33f5c40ca861719689fde68af46
Manu de Quevedo <hola@manudequevedo.com>
2019-09-15 10:23:28 -0500

    Added contact form test1

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -36,6 +36,27 @@ export default connect(mapStateToProps)(props => {
           </div>
           <div className="grid-1">
             <h1>Form is here</h1>
+            <form name="contact" method="post" data-netlify="true">
+              <input type="hidden" name="form-name" value="contact" />
+              <p>
+                <label>
+                  Your Name: <input type="text" name="name" />
+                </label>
+              </p>
+              <p>
+                <label>
+                  Your Email: <input type="email" name="email" />
+                </label>
+              </p>
+              <p>
+                <label>
+                  Message: <textarea name="message"></textarea>
+                </label>
+              </p>
+              <p>
+                <button type="submit">Send</button>
+              </p>
+            </form>
           </div>
         </div>
       </div>
